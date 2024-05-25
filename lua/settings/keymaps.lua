bfa010662cd86552e4df7b33d078fe65f94d72e8
nanahiryu <ryu2751x7@gmail.com>
2024-05-25 12:26:03 +0900

    cocの保管のためのkeymaps更新

--- lua/settings/keymaps.lua
+++ lua/settings/keymaps.lua
@@ -1,11 +1,15 @@
+local keyset = vim.keymap.set
+local opts = { noremap = true, silent = true, expr = true, replace_keycodes = false }
+
 -- telescope
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', { noremap = true, silent = true })
+keyset('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
+keyset('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', opts)
+keyset('n', '<leader>fb', '<cmd>Telescope buffers<cr>', opts)
+keyset('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', opts)
 
 -- 補完表示時にEnterで補完
-vim.opt.completeopt = {'menuone', 'noinsert'}
-vim.api.nvim_set_keymap('i', '<CR>', [[coc#pum#visible() ? coc#pum#confirm() : "\<CR>"]], {expr = true, silent = true})
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+keyset("i", "<CR>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
 
